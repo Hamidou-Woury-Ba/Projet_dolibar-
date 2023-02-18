1ad1dfe2e0db11ebeb219ca1dd9053011a6df803
diara-bs <124560110+diara-bs@users.noreply.github.com>
2023-02-18 11:33:49 +0000

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mon_projet/main.dart';
+import 'package:projet_dolibar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
